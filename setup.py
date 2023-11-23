78816098be715d7b79c5c4526938c00db5615449
Lukas B. Nordentoft <Lukasbnordentoft@gmail.com>
2023-11-23 15:27:36 +0100

    cleanup

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(name='PyMAA',
-      version='0.1.2',
+      version='0.1.3',
       description='A Python library for Modeling All Alternatives',
       url='https://github.com/LukasBNordentoft/PyMAA_LBN',
-      download_url = 'https://github.com/LukasBNordentoft/PyMAA/archive/refs/tags/v0.1.2.tar.gz',
+      download_url = 'https://github.com/LukasBNordentoft/PyMAA/archive/refs/tags/v0.1.3.tar.gz',
       author='Tim Pedersen',
       author_email='timtoernes@gmail.com',
       maintainer = 'Lukas B. Nordentoft',
