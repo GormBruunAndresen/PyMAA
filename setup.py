7c5d7f47b6169775e5938947d024615f462a495a
Lukas B. Nordentoft <Lukasbnordentoft@gmail.com>
2024-02-07 14:50:59 +0100

    Incremented version

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(name='PyMAA',
-      version='0.1.7',
+      version='0.1.8',
       description='A Python library for Modeling All Alternatives',
       url='https://github.com/LukasBNordentoft/PyMAA_LBN',
-      download_url = 'https://github.com/LukasBNordentoft/PyMAA/archive/refs/tags/v0.1.7.tar.gz',
+      download_url = 'https://github.com/LukasBNordentoft/PyMAA/archive/refs/tags/v0.1.8.tar.gz',
       author='Tim Pedersen',
       author_email='timtoernes@gmail.com',
       maintainer = 'Lukas B. Nordentoft',
